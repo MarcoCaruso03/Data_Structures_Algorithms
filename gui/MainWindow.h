6c5a96c20997b0103841c2d3fa58c74a27b63f93
Marco Caruso <marcocaruso321@gmail.com>
2025-10-26 10:56:56 +0100

    More attempts in Qt

--- gui/MainWindow.h
+++ gui/MainWindow.h
@@ -1,11 +1,15 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+
+
 #include <QMainWindow>
+#include <QPushButton>
 #include <QTextEdit>
 #include <QLineEdit>
-#include <QPushButton>
-#include <QAction>
+#include <QVBoxLayout>
+#include <QStringList>
+#include "ui_mainwindow.h" // generato da Qt Designer
 
 class MainWindow : public QMainWindow
 {
@@ -13,24 +17,13 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
 private slots: 
-    void addText();       // inserisce testo da QLineEdit a QTextEdit
-    void newFile();       // pulisce QTextEdit
-    void exitApp();       // chiude app
-    void showAbout();     // QMessageBox informativo
-    void setRedText();    // cambia colore testo in rosso
-    void setBlueText();   // cambia colore testo in blu
-
+    void aggiungiDato();     // aggiunge un nuovo dato
+    void aggiornaGUI();      // aggiorna il QTextEdit
 
 
 private: 
-    QTextEdit * textEdit; 
-    QPushButton * button; 
-    QLineEdit * input; 
-    QAction *newAction;
-    QAction *exitAction;
-    QAction *aboutAction;
-    QAction *redAction;
-    QAction *blueAction;
+    Ui::MainWindow ui;  // oggetto generato da Qt Designer
+    QStringList listaDati;
 
 };
 
